5ad1b0c8adacf1c2b41f0d1378ca246beacdc015
rohkulkagit <ms.rohini.kulkarni@gmail.com>
2020-01-31 12:12:57 +0530

    First commit

---
+++ src/test/java/interview/TestCasesSet1/SearchTests.java
@@ -0,0 +1,24 @@
+package interview.TestCasesSet1;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SearchTests {	
+
+		
+	@Test(enabled = true,priority=2)
+	@Parameters({"browser","url"})
+	public void testmethod11(@Optional String browser,String url){
+		System.out.println("test method testmethod11");
+		System.out.println("browser = "+browser);
+		System.out.println("url = "+url);
+	}
+
+	
+	@Test(enabled = true, priority=1)
+	public void testmethod12(){
+		System.out.println("test method testmethod12");
+	}
+	
+}
